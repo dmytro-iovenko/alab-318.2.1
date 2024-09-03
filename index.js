b891d376ea0437ab1e4181d04d099674ef6c39f3
Dmytro Iovenko <iovenko@gmail.com>
2024-09-03 15:19:34 -0400

    set the view engine to ejs

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}.`);
 });
