3417b2a677da88330a56142ca2d51fe566b9310a
Dmytro Iovenko <iovenko@gmail.com>
2024-09-03 15:20:46 -0400

    load up index.ejs view file

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = 3000;
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+// use res.render to load up an ejs view file
+// index page
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}.`);
 });
