1685041707efc9f87c4629bd2b883b70340f9adf
Dmytro Iovenko <iovenko@gmail.com>
2024-09-03 15:23:29 -0400

    add about page

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+/** Part 1: Routes, Templates, and Views */
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 // use res.render to load up an ejs view file
+
 // index page
 app.get('/', function(req, res) {
   res.render('index');
 });
+
+// about page
+app.get('/about', function(req, res) {
+  res.render('about');
+});
+
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}.`);
